b5bb81e92ae8b798d7e0f2b2e0b93a28b0328800
Charles Wheeler <charles.wheeler@bigfishgames.com>
2019-12-04 14:01:01 -0800

    Starting to implement the Intcode module

---
+++ 02/day2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys, os
+sys.path.append( os.path.dirname("../lib/") )
+
+from Intcode import Intcode
+
+ic = Intcode()
---
+++ lib/Intcode.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+class Intcode():
+    def __init__(self, *args):
+        self.origin = []
+        # Handle 
