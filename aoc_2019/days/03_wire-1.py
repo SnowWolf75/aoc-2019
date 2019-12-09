60dd3ade5d0a858fe2434820bdbd42df88e2d0d1
Charles Wheeler <charles.wheeler@bigfishgames.com>
2019-12-08 19:43:41 -0800

    Major structure reorg

---
+++ aoc_2019/days/03_wire-1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import sys, os
+sys.path.append( os.path.dirname("../lib/") )
+
---
+++ get-input.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import argparse
+import subprocess
+from datetime import datetime
+
+cur_day = datetime.now().day
+cur_year = datetime.now().year
+
+parser = argparse.ArgumentParser(description='Read Input for Advent of Code')
+parser.add_argument('--day', type=int, default=cur_day)
+parser.add_argument('--year', type=int, default=cur_year)
+args = parser.parse_args()
+
+cmd = 'curl https://adventofcode.com/{}/day/{}/input --cookie "session=53616c7465645f5f929b0f8abf13f82ed097688a169ddc8ffacbc8e9aef59bba3f8d4186d54568eb41b354fef3143ebc"'.format(
+    args.year, args.day)
+output = subprocess.check_output(cmd, shell=True)
+print(output,)
+
+outfile = "aoc_2019/inputs/{0:04d}_12_{1:02d}_input.txt".format( args.year, args.day )
+with open(outfile, "w") as fh:
+    fh.write(str(output))
+
+# https://adventofcode.com/2019/day/8/input
