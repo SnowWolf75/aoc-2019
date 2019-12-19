e34dad1a0334e1c7b3eaaa7c033b27768c0b1df8
Charles Wheeler <charles.wheeler@bigfishgames.com>
2019-12-19 11:14:17 -0800

    19th

---
+++ 18.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys, os
+import unittest
+from lib.common import *
+
+
+filename = "inputs/2019_12_18_input.txt"
+
+class day18:
+  def __init__(self):
+    pass
+
+class day18part1(day18):
+  def solve(self, args):
+    pass
+
+class day18part2(day18):
+  def solve(self, args):
+    pass
+
+
+class examples(unittest.TestCase):
+  def test_examples_part1(self):
+    day18 = day18part1()
+    # self.assetTrue()
+
+  def test_examples_part2(self):
+    day18 = day18part2()
+    # self.assetTrue()
+
+class solutions(unittest.TestCase):
+  def test_part1(self):
+    day18 = day18part1()
+
+  def test_part2(self):
+    day18 = day18part2()
+
---
+++ 19.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys, os
+import unittest
+from lib.common import *
+
+
+filename = "inputs/2019_12_19_input.txt"
+
+class day19:
+  def __init__(self):
+    pass
+
+class day19part1(day19):
+  def solve(self, args):
+    pass
+
+class day19part2(day19):
+  def solve(self, args):
+    pass
+
+
+class examples(unittest.TestCase):
+  def test_examples_part1(self):
+    day19 = day19part1()
+    # self.assetTrue()
+
+  def test_examples_part2(self):
+    day19 = day19part2()
+    # self.assetTrue()
+
+class solutions(unittest.TestCase):
+  def test_part1(self):
+    day19 = day19part1()
+
+  def test_part2(self):
+    day19 = day19part2()
+
